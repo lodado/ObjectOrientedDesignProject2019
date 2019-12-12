b205e58ad7f6c03051c0a6a137d608ff0ef18007
ycp998 <ycp998@naver.com>
2019-12-12 20:39:00 +0900

    Revert "수정"
    This reverts commit 923b0422d90d1fcfdb60a6d4987957371e6c2323.

--- src/playground/CharacterSelectView.java
+++ src/playground/CharacterSelectView.java
@@ -38,10 +38,26 @@ public class CharacterSelectView{
 		frame.setTitle("Text Battle");
 		frame.setSize(500, 500);
 		frame.setResizable(false);
+		/*
+		ImageIcon ic1 = new ImageIcon("./cha1.jpg");
+		ImageIcon ic2 = new ImageIcon("./cha2.jpg");
+		ImageIcon ic3 = new ImageIcon("./cha3.jpg");
+		Image im1 = ic1.getImage();
+		Image im2 = ic2.getImage();
+		Image im3 = ic3.getImage();
+		Image nim1 = im1.getScaledInstance(150, 300, Image.SCALE_DEFAULT);
+		Image nim2 = im2.getScaledInstance(150, 300, Image.SCALE_DEFAULT);
+		Image nim3 = im3.getScaledInstance(150, 300, Image.SCALE_DEFAULT);
+		ImageIcon icon1 = new ImageIcon(nim1);
+		ImageIcon icon2 = new ImageIcon(nim2);
+		ImageIcon icon3 = new ImageIcon(nim3);
+		*/
 
 		ImageIcon ic1 = (new ImageIcon(getClass().getClassLoader().getResource("image/cha1.png")));
 		ImageIcon ic2  = (new ImageIcon(getClass().getClassLoader().getResource("image/cha2.png")));
 		ImageIcon ic3 = (new ImageIcon(getClass().getClassLoader().getResource("image/cha3.png")));
+		
+		
 		
 		Image im1 = ic1.getImage();
 		Image im2 = ic2.getImage();
@@ -50,9 +66,12 @@ public class CharacterSelectView{
 		Image nim2 = im2.getScaledInstance(150, 300, Image.SCALE_DEFAULT);
 		Image nim3 = im3.getScaledInstance(150, 300, Image.SCALE_DEFAULT);
 		
+		
 		JLabel lbImage1 = new JLabel(new ImageIcon(nim1));
 		JLabel lbImage2 = new JLabel(new ImageIcon(nim2));
 		JLabel lbImage3 = new JLabel(new ImageIcon(nim3));
+		
+		
 		
 		lbImage1.setBounds(0, 0, 150, 300);
 		lbImage2.setBounds(175, 0, 150, 300);
@@ -68,9 +87,10 @@ public class CharacterSelectView{
 		dCha2.setLocation(175, 330);
 		dCha3.setLocation(325, 330);
 
+		// add panel to frame
 		ButtonPanel buttonPanel = new ButtonPanel(frame, user, player);
 		buttonPanel.setLayout(null);
-
+		// ImagePanel imagePanel = new ImagePanel();
 		Container contentPane = frame.getContentPane();
 
 		contentPane.add(dCha1);
@@ -80,8 +100,10 @@ public class CharacterSelectView{
 		contentPane.add(lbImage2);
 		contentPane.add(lbImage3);
 		contentPane.add(buttonPanel);
-
+		//보이기
 		frame.setVisible(true);
+		
+		// contentPane.add(panel3);
 	}
 }
 
@@ -113,6 +135,7 @@ class ButtonPanel extends JPanel implements ActionListener {
 		cha2 = new GameCharacter("공격형", 80, 15, 5, 10, "image/cha2.png");
 		cha3 = new GameCharacter("방어형", 120, 5, 15, 10, "image/cha3.png");
 
+		// create buttons
 		selectButton1 = new JButton("Select");
 		selectButton2 = new JButton("Select");
 		selectButton3 = new JButton("Select");
@@ -123,7 +146,7 @@ class ButtonPanel extends JPanel implements ActionListener {
 		selectButton1.setLocation(0, 370);
 		selectButton2.setLocation(175, 370);
 		selectButton3.setLocation(325, 370);
-
+		// add buttons to panel
 		selectButton1.addActionListener(this);
 		selectButton2.addActionListener(this);
 		selectButton3.addActionListener(this);
